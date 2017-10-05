abb6ffec02d8f06b9c3fa47ff4ed4ff0abace331
deytonk <deyton.koch@gmail.com>
2017-10-05 16:47:28 -0500

    caught up with class work whiteboarding

--- 01week/dataTypes.js
+++ 01week/dataTypes.js
@@ -16,14 +16,24 @@
 // Write a JavaScript program that runs when 1 of 2 things are true.
 // Write a JavaScript program that runs when both things are not true.
 
-// WHITEBOARDING
+// WHITEBOARDING:
 
 // Display the current day and time:
 //   variable: weekDay - array of 0 between 6
 //   give each weekday value the name of the weekdays in order
 //   get.day() method to retrieve current day's number
 //   display weekDay
-//   variable: time = getTime() Which only displays the number of milliseconds since January 1, 1970. 
+//   variable: time = getTime() Which only displays the number of milliseconds since January 1, 1970.
+// Convert number to string:
+//   Make a variable called number and give it a number value.
+//   Use the num.toString() method to convert said number.
+// Convert string to number:
+//   Make a variable called string and give it a string value.
+//   Use the parseInt() or the parseFloat() mehtods to convert the string value into a number.
+// Detects and prints what kind of datatype something is 
+//   typeOf() method
+
+// CODE:
 
 // not sure why the first variable is necessary to get it to work?
 var d = new Date();

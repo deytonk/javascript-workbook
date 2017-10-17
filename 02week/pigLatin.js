8c591df68255b37f30b625e7f7fbcfb9c0343739
deytonk <deyton.koch@gmail.com>
2017-10-17 11:55:40 -0500

    passing 2/4 tests, so added trim method and now passing 3/4 tests! :thumbsup:

--- 02week/pigLatin.js
+++ 02week/pigLatin.js
@@ -15,16 +15,16 @@ function pigLatin(word) {
   // WHITE BOARDING:
   // Find position of first vowel (a,e,i,o,u, or y)
 
-  word = word.toLowerCase();
+  word = word.trim().toLowerCase();
   // look up regex expressions .match(string)
   const firstVowel = word.match(/[aeiouy]/);
   const firstPosition = word.indexOf(firstVowel);
 
   // If vowel is the first letter of the word, return the word with 'ay' at the end, unless Y is the first letter, then treat as other words
   if (firstPosition > -1) {
+    // For all other words, return any letters before the vowel to the end of the word with 'ay' at the end, moving the first vowel to position 0
     return word.slice(firstPosition) + word.slice(0, firstPosition) + "ay";
   }
-  // For all other words, return any letters before the vowel to the end of the word with 'ay' at the end, moving the first vowel to position 0
 }
 
 

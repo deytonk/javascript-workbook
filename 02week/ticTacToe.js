021618f1973fa1bf0fa9ade0cbc5e742029e0020
deytonk <deyton.koch@gmail.com>
2017-10-19 20:26:13 -0500

    completed whiteboarding :sunglasses:

--- 02week/ticTacToe.js
+++ 02week/ticTacToe.js
@@ -25,23 +25,36 @@ function printBoard() {
 
 function horizontalWin() {
   // Your code here
+  // in row, column format these combinations of matching x's or o's will return a win
+  // 00 01 02
+  // 10 11 12
+  // 20 21 22
 }
 
 function verticalWin() {
   // Your code here
+  // in row, column format these combinations of matching x's or o's will return a win
+  // 00 10 20
+  // 01 11 21
+  // 02 12 22
 }
 
 function diagonalWin() {
   // Your code here
+  // in row, column format these combinations of matching x's or o's will return a win
+  // 00 11 22
+  // 02 11 20
 }
 
 function checkForWin() {
   // Your code here
+  // if in any of the above combinations, there are either all x's or all o's then the respective player wins
 }
 
 function ticTacToe(row, column) {
   // Your code here
-  console.log(row, column)
+  // the playerturn will start at x but will alternate between xs and os onclicks, and for each click it will checkForWin.
+  // if win, game winner will be announced and board will reset.
 }
 
 function getPrompt() {

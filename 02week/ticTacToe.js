874621ab5e5eee10b534c014eb2f959bcf090f26
deytonk <deyton.koch@gmail.com>
2017-10-21 10:40:07 -0500

    just making a big mess of things :turtle:

--- 02week/ticTacToe.js
+++ 02week/ticTacToe.js
@@ -24,60 +24,78 @@ function printBoard() {
   console.log('2 ' + board[2].join(' | '));
 }
 
-// function horizontalWin() {
+function horizontalWin() {
   // Your code here
   // in row, column format these combinations of matching x's or o's will return a win
   // 00 01 02
   // 10 11 12
   // 20 21 22
-  // if (([0,0] === [0,1] === [0,2]) || ([1,0] === [1,1] === [1,2]) || ([2,0] === [2,1] === [2,2])) {
-  //   return "Player ${} Wins!";
-  // }
+  if (([0,0] === [0,1] === [0,2]) || ([1,0] === [1,1] === [1,2]) || ([2,0] === [2,1] === [2,2])) {
+    return `Player ${playerTurn} Wins!`;
+  }
+}
 
-// }
-
-// function verticalWin() {
+function verticalWin() {
   // Your code here
   // in row, column format these combinations of matching x's or o's will return a win
   // 00 10 20
   // 01 11 21
   // 02 12 22
-  // if (([0,0] === [1,0] === [2,0]) || ([0,1] === [1,1] === [2,1]) || ([0,2] === [1,2] === [2,2])) {
-  //   return "Player ${} Wins!";
-  // }
-// }
+  if (([0,0] === [1,0] === [2,0]) || ([0,1] === [1,1] === [2,1]) || ([0,2] === [1,2] === [2,2])) {
+    return `Player ${playerTurn} Wins!`;
+  }
+}
 
-// function diagonalWin() {
+function diagonalWin() {
   // Your code here
   // in row, column format these combinations of matching x's or o's will return a win
   // 00 11 22
   // 02 11 20
-  // if (([0,0] === [1,1] === [2,2]) || ([0,2] === [1,1] === [2,0])) {
-  //   return "Player ${} Wins!"
-  // }
-// }
-
-winningCombinations = () => {
-  [
-    // horizontal wins
-    [0, 1, 2], [3, 4, 5], [6, 7, 8],
-    // verticals wins
-    [0, 3, 6], [1, 4, 7], [2, 5, 8],
-    // diagonal wins
-    [0, 4, 8], [2, 4, 6]
-  ];
+  if (([0,0] === [1,1] === [2,2]) || ([0,2] === [1,1] === [2,0])) {
+    return `Player ${playerTurn} Wins!`;
+  }
 }
+
+const winningCombinations = horizontalWin() || verticalWin() || diagonalWin();
+
+// winningCombinations = (row, column) => {
+//   [
+//     // horizontal wins
+//     [[0,0], [0,1], [0,2]], [[1,0], [1,1], [1,2]], [[2,0], [2,1], [2,2]],
+//     // verticals wins
+//     [[0,0], [1,0], [2,0]], [[0,1], [1,1], [2,1]], [[0,2], [1,2], [2,2]],
+//     // diagonal wins
+//     [[0,0], [1,1], [2,2]], [[0,2], [1,1], [2,0]];
+//   ];
+// }
 
 function checkForWin(playerTurn) {
   // Your code here
   // if in any of the above combinations, there are either all x's or all o's then the respective player wins
   // on each click, if horizontalWin or verticalWin or diagonalWin - gameover and ${} player wins. If no win, then alternate player.
+  for (var i = 0; i < winningCombinations.length; i++) {
+    let i, j
+    let count = 0;
+    for (let j = 0; j < winningCombinations[i].length; j++) {
+    if (board[winningCombinations[i][j]] === playerTurn) {
+        count++;
+    }if (count === 3) {
+        return true;
+    }
+  }
+  return false;
 }
 
 function ticTacToe(row, column) {
   // Your code here
   // the playerturn will start at x but will alternate between xs and os onclicks, and for each click it will checkForWin.
-  // if win, game winner will be announced and board will reset.
+    // if win, game winner will be announced and board will reset.
+  // if (playerTurn === 'X') {
+  //   playTurn('O');
+  // } else {
+  //   playTurn('X');
+  }
+
 }
 
 function getPrompt() {

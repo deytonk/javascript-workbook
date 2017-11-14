c93a6d12874f24b709d331b18a5ef23b70991ca9
deytonk <deyton.koch@gmail.com>
2017-11-14 12:23:41 -0600

    Passing 6 :fish:

--- 06week/functionalJavaScript/06basicReduce.js
+++ 06week/functionalJavaScript/06basicReduce.js
@@ -3,14 +3,20 @@
 // Given an Array of strings, use Array#reduce to create an object that contains the number of times each string occured in the array.
 // Return the object directly (no need to console.log).
 
+// Counting instances of values in an object
+
 const countWords = (inputWords) => {
-  // let obj = {};
-  inputWords.reduce((acc, i) => {
-    if (!acc[i]) {
-    acc[i] = 0;
-    acc[i]++;
+  let countedWords = inputWords.reduce((acc, words) => {
+    if (words in acc) {
+      // acc[words] = 0;
+      acc[words]++;
+    } else {
+      acc[words] = 1;
+    }
     return acc;
-  } , {}});
+  }, {});
+
+  return countedWords;
 }
 
 module.exports = countWords

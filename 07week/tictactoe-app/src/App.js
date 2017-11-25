d5d2d369cb928a90af6848ba6b85aa32c8377b35
deytonk <deyton.koch@gmail.com>
2017-11-25 05:55:09 -0600

    Not much progress from meet-up

--- 07week/tictactoe-app/src/App.js
+++ 07week/tictactoe-app/src/App.js
@@ -11,7 +11,7 @@ class Square extends React.Component {
   }
   render() {
     return (
-      <button className="square" onClick={() => this.props.onClick()}>
+      <button className="square" onClick = {() => this.props.onClick()}>
         {this.props.value}
       </button>
     );

bc537594e068af73ab8d5cbafbd2a19c18b7acaa
deytonk <deyton.koch@gmail.com>
2017-11-28 15:29:21 -0600

    functioning tutorial

--- 07week/tictactoe-app/src/App.js
+++ 07week/tictactoe-app/src/App.js
@@ -2,32 +2,46 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class Square extends React.Component {
-  constructor (props) {
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
+
+// The Square class makes each square on the Board class a button that alternates between placing X's and O's on the board when clicked
+// class Square extends React.Component {
+  // constructor (props) {
+  //   super(props);
+  //   this.state = {
+  //     value: null,
+  //   };
+  // }
+
+  // (Sqare class is considered a 'contolled component' because it no longer keeps its own state)
 
   /*state = {
    new way to write that ^
   }*/
 
-  render() {
-    return (
-      <button className="square" onClick = {() => this.setState({value: 'X'})}>
-        {this.state.value}
-      </button>
-    );
-  }
+//   render() {
+//     return (
+//       <button className="square" onClick = {() => this.props.onClick()}>
+//         {this.props.value}
+//       </button>
+//     );
+//   }
+// }
+
+const Square = (props) => {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
 }
 
+// The Board class renders the board game on the page
 class Board extends React.Component {
   constructor (props) {
     super(props);
     this.state = {
-
+      squares: Array(9).fill(null),
+      nextPlayer: true,
     };
   }
 
@@ -35,12 +49,37 @@ class Board extends React.Component {
     new way to write that ^
   }*/
 
+  handleClick(i) {
+    // (slice to copy the squares array instead of mutating the existing array)
+    const squares = this.state.squares.slice();
+    if (checkForWin(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.nextPlayer ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      nextPlayer: !this.state.nextPlayer,
+    });
+  }
+
   renderSquare(i) {
-    return <Square  value={i}/>;
+    // set up a click even listener that calls the onclick event handler in the Square class
+    return (
+      <Square
+        value = {this.state.squares[i]}
+        onClick = {() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
-    const status = 'Next player: X';
+    const winner = checkForWin(this.state.squares);
+    let status;
+    if (winner) {
+      status = winner + ' is the winner!';
+    } else {
+      status = 'Next player: ' + (this.state.nextPlayer ? 'X' : 'O');
+    }
 
     return (
       <div>
@@ -65,19 +104,21 @@ class Board extends React.Component {
   }
 }
 
+// The Game class will dictate the flow of the game: check for win / announce winner and alternate player turns
 class Game extends React.Component {
-  constructor (props) {
-    super(props);
-    this.state = {
-
-    };
-  }
+  // constructor (props) {
+  //   super(props);
+  //   this.state = {
+  //
+  //   };
+  // }
 
   /*state = {
     new way to write that ^
   }*/
 
   render() {
+
     return (
       <div className="game">
         <div className="game-board">
@@ -91,6 +132,28 @@ class Game extends React.Component {
     );
   }
 }
+
+
+  // Either figure how the f this shit works or rewrite it in a way that I understand 
+  const checkForWin = (squares) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
 
 
 export default Square && Board && Game;

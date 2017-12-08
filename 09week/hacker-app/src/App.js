994bc58fc9c24ca0a4ba17d8005d2c9cdef2f78f
deytonk <deyton.koch@gmail.com>
2017-12-07 18:28:57 -0600

    progress before class

--- 09week/hacker-app/src/App.js
+++ 09week/hacker-app/src/App.js
@@ -7,17 +7,28 @@ class App extends Component {
   }
 
   componentDidMount(){
-    fetch
-    this.setState({stories: [...this.state.stories, story]})
-    // const newState = this.state.storires;
-    // newState.push(story);
-    // this.setState({stories: newState})
+    fetch('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty')
+    .then(res => res.json())
+    .then(storyIds => {
+      storyIds.slice(0, 10).forEach((storyId) => {
+        fetch(`https://hacker-news.firebaseio.com/v0/item/${storyId}.json?print=pretty`)
+        .then(res => res.json())
+    // .then(story => {
+    //   this.setState({stories: [...this.state.stories, story]})
+  // const newState = this.state.storires;
+  // newState.push(story);
+  // this.setState({stories: newState})
+        console.log(story);
+      });
+    });
   }
+
   renderStories(){
     return this.state.stories.map((story, key) => {
       return <li key={key}>{story.title}</li>
-    })
+    });
   }
+
   render() {
 
     return (
